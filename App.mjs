2cb0f1dd279a8326ef8ba271872db00a70164df7
Qqkyu <kulmisiu007@gmail.com>
2021-04-17 18:55:35 +0200

    Added algorithms for counting users posts and finding duplicate topic titles

--- App.mjs
+++ App.mjs
@@ -1,1 +1,29 @@
 import requestMatches from "./src/Match.mjs";
+import requestPosts from "./src/Posts.mjs";
+
+async function usersPostsCount() {
+    const [matches, infos] = [await requestMatches(), []];
+    for (const userId in matches) {
+        const user = matches[userId];
+        infos.push(
+            `${user["username"]} napisał(a) ${user["posts"].length} postów`
+        );
+    }
+    return infos;
+}
+
+async function duplicateTopicTitles() {
+    const [posts, titles, duplicateTitles] = [await requestPosts(), [], []];
+
+    for (let { title } of posts) {
+        if (!titles.includes(title)) {
+            titles.push(title);
+        } else if (!duplicateTitles.includes(title)) {
+            duplicateTitles.push(title);
+        }
+    }
+    console.log(duplicateTitles);
+    return duplicateTitles;
+}
+
+console.log(await duplicateTopicTitles());
